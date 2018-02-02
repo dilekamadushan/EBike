475f9af4bc84fa90b7874f92ea4bcc6a8e151781
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2018-02-02 12:07:43 +0530

    initiated the settings page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import {LoginPage} from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import {LoginPage} from "../pages/login/login";
+
 
 @NgModule({
   declarations: [
