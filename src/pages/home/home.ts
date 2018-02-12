2dc752f1256628d362d05942e0611ec78c280725
yasgun <yasas.14@cse.mrt.ac.lk>
2018-02-12 20:23:29 +0530

    Home page ui changes still in progress

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,6 +17,106 @@ export class HomePage {
   }
 
   ionViewDidLoad() {
+    /* global document */
+// Load the fonts
+    HighCharts.createElement('link', {
+      href: 'https://fonts.googleapis.com/css?family=Signika:400,700',
+      rel: 'stylesheet',
+      type: 'text/css'
+    }, null, document.getElementsByTagName('head')[0]);
+
+// Add the background image to the container
+    HighCharts.wrap(HighCharts.Chart.prototype, 'getContainer', function (proceed) {
+      proceed.call(this);
+      this.container.style.background =
+        'transparent';
+      this.container.style.width = '100%';
+    });
+
+    HighCharts.theme = {
+      colors: ['#f45b5b', '#8085e9', '#8d4654', '#7798BF', '#aaeeee',
+        '#ff0066', '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+      chart: {
+        backgroundColor: null,
+        style: {
+          fontFamily: 'Signika, serif'
+        }
+      },
+      title: {
+        style: {
+          color: 'black',
+          fontSize: '16px',
+          fontWeight: 'bold'
+        }
+      },
+      subtitle: {
+        style: {
+          color: 'black'
+        }
+      },
+      tooltip: {
+        borderWidth: 0
+      },
+      legend: {
+        itemStyle: {
+          fontWeight: 'bold',
+          fontSize: '13px'
+        }
+      },
+      xAxis: {
+        labels: {
+          style: {
+            color: '#6e6e70'
+          }
+        }
+      },
+      yAxis: {
+        labels: {
+          style: {
+            color: '#6e6e70'
+          }
+        }
+      },
+      plotOptions: {
+        series: {
+          shadow: true
+        },
+        candlestick: {
+          lineColor: '#404048'
+        },
+        map: {
+          shadow: false
+        }
+      },
+
+      // Highstock specific
+      navigator: {
+        xAxis: {
+          gridLineColor: '#D0D0D8'
+        }
+      },
+      rangeSelector: {
+        buttonTheme: {
+          fill: 'white',
+          stroke: '#C0C0C8',
+          'stroke-width': 1,
+          states: {
+            select: {
+              fill: '#D0D0D8'
+            }
+          }
+        }
+      },
+      scrollbar: {
+        trackBorderColor: '#C0C0C8'
+      },
+
+      // General
+      background2: '#E0E0E8'
+
+    };
+    // Apply the theme
+    HighCharts.setOptions(HighCharts.theme);
     HighCharts.chart('speedometer-container', {
         chart: {
           type: 'gauge',
@@ -38,10 +138,10 @@ export class HomePage {
 
         yAxis: [{
           min: 0,
-          max: 200,
-          lineColor: '#339',
-          tickColor: '#339',
-          minorTickColor: '#339',
+          max: 25,
+          lineColor: '#083d77',
+          tickColor: '#083d77',
+          minorTickColor: '#083d77',
           offset: -25,
           lineWidth: 2,
           labels: {
@@ -53,13 +153,13 @@ export class HomePage {
           endOnTick: false
         }, {
           min: 0,
-          max: 124,
+          max: 40,
           tickPosition: 'outside',
-          lineColor: '#933',
+          lineColor: '#f95738',
           lineWidth: 2,
           minorTickPosition: 'outside',
-          tickColor: '#933',
-          minorTickColor: '#933',
+          tickColor: '#f95738',
+          minorTickColor: '#f95738',
           tickLength: 5,
           minorTickLength: 5,
           labels: {
@@ -105,10 +205,10 @@ export class HomePage {
           if (chart.axes) { // not destroyed
             var point = chart.series[0].points[0],
               newVal,
-              inc = Math.round((Math.random() - 0.5) * 20);
+              inc = Math.round((Math.random() - 0.5) * 5);
 
             newVal = point.y + inc;
-            if (newVal < 0 || newVal > 200) {
+            if (newVal < 0 || newVal > 40) {
               newVal = point.y - inc;
             }
 
